90dd7ee856072d3f4d21e44698b734605663a417
wlsdks <wlsdks12@naver.com>
2024-11-21 22:53:07 +0900

    :sparkles: read 엔티티 추가 (계좌정보)

---
+++ src/main/java/com/example/cqrs/entity/read/AccountReadEntity.java
@@ -0,0 +1,39 @@
+package com.example.cqrs.entity.read;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 읽기 전용 계좌 모델입니다.
+ * CQRS의 읽기 모델 측면에서 올바르게 구현된 것입니다.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+@Table(name = "account_read")
+public class AccountReadEntity {
+
+    @Id
+    private String accountId; // 계좌 ID
+
+    @Column(name = "balance")
+    private double balance;   // 잔액
+
+    // factory method
+    public static AccountReadEntity of(String accountId, Double amount) {
+        return new AccountReadEntity(accountId, amount);
+    }
+
+    // 잔액 변경 메서드
+    public void changeBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
